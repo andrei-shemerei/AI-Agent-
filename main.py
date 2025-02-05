0f8705d224a9dbdce438f5b7ed00b33a4be701bf
unknown <andrejsemerej537@gmail.com>
2025-02-05 13:04:57 +0300

    Change to Gemini model.
    Create code generation for dataframe

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 from dotenv import load_dotenv
 import os
 import pandas as pd 
+from prompts import new_prompt, instruction_str
+import google.generativeai as genai
 
 load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=GOOGLE_API_KEY)
+
 
 csv_data_path = os.path.join("data", "WorldPopulation2023.csv")
 population_df = pd.read_csv(csv_data_path)
 
-print(population_df.head(5))
+
+def generate_pandas_code(query, df):
+    "code generation"
+    df_str = df.head().to_string()
+    prompt_text = new_prompt.format(df_str=df_str, instruction_str=instruction_str, query_str=query)
+    
+    prompt_text = prompt_text.replace("df", "population_df") 
+    print(f"Prompt text: \n{prompt_text}\n")
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt_text)
+    
+    return response.text.strip()
+
+
+def query_population(query):
+   
+    code = generate_pandas_code(query, population_df)
+    
+    print(f"Generated code: {code}")
+    
+    try:
+        local_vars = {"population_df": population_df}
+        exec(f"result = {code}", {}, local_vars)
+        return local_vars.get("result", "No result").item()
+    except Exception as e:
+        return f"Error executing code: {str(e)}"
+
+
+query = "What is the population of Zambia?"
+result = query_population(query)
+
+print(f"Query result: {result}")
