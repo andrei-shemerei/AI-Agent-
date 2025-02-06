e058c8ffb422c1ee5648c77e5502850ab761cc46
unknown <andrejsemerej537@gmail.com>
2025-02-06 11:57:04 +0300

    Impement pandas and notes tools

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
 from dotenv import load_dotenv
 import os
 import pandas as pd 
-from prompts import new_prompt, instruction_str
-import google.generativeai as genai
+from llama_index.llms.gemini import Gemini
+from llama_index.core import Settings
+from llama_index.experimental.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str, context
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from note_engine import note_engine
+#import google.generativeai as genai
 
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=GOOGLE_API_KEY)
+#genai.configure(api_key=GOOGLE_API_KEY)
 
+llm = Gemini()
+Settings.llm = llm
 
 csv_data_path = os.path.join("data", "WorldPopulation2023.csv")
 population_df = pd.read_csv(csv_data_path)
 
+population_query_engine = PandasQueryEngine(
+    df=population_df, verbose=True, instruction_str=instruction_str
+)
 
-def generate_pandas_code(query, df):
-    "code generation"
-    df_str = df.head().to_string()
-    prompt_text = new_prompt.format(df_str=df_str, instruction_str=instruction_str, query_str=query)
+population_query_engine.update_prompts({"pandas_prompt" : new_prompt})
+
+tools = [
+    QueryEngineTool(
+        query_engine=population_query_engine,
+        metadata=ToolMetadata(
+            name="population_data",
+            description="this gives information at the world population and demographics",
+        ),
+    ),
+    note_engine
+]
+
+agent = ReActAgent.from_tools(
+    tools=tools,
+    llm=llm,
+    verbose=True,
+    context=context,
+)
+
+
+while True:
+    prompt = input("Enter a prompt (or tupe 'q' to exit): ")
+    if prompt == "q":
+        print("Programm is existing")
+        break
+
+    result = agent.query(prompt)
+    print(result)
     
-    prompt_text = prompt_text.replace("df", "population_df") 
-    print(f"Prompt text: \n{prompt_text}\n")
-    model = genai.GenerativeModel("gemini-pro")
-    response = model.generate_content(prompt_text)
-    
-    return response.text.strip()
-
-
-def query_population(query):
-   
-    code = generate_pandas_code(query, population_df)
-    
-    print(f"Generated code: {code}")
-    
-    try:
-        local_vars = {"population_df": population_df}
-        exec(f"result = {code}", {}, local_vars)
-        return local_vars.get("result", "No result").item()
-    except Exception as e:
-        return f"Error executing code: {str(e)}"
-
-
-query = "What is the population of Zambia?"
-result = query_population(query)
-
-print(f"Query result: {result}")
